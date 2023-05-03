6c328013ef96a7bf9df02dc76c3902bbaeeea340
Musthafa Faishal <musthafafaishal@gmail.com>
2023-05-03 09:33:18 +0700

    refactor: change the list story field data type

--- app/src/main/java/com/dicoding/storyapp/data/remote/response/GetAllStoriesResponse.kt
+++ app/src/main/java/com/dicoding/storyapp/data/remote/response/GetAllStoriesResponse.kt
@@ -10,5 +10,5 @@ data class GetAllStoriesResponse(
     val message: String,
 
     @field:SerializedName("listStory")
-    val listStory: ArrayList<ListStoryResponse>
+    val listStory: ArrayList<StoryResponse>
 )
