f7a9f3e8559327f645fc3515c858775c289ca334
Musthafa Faishal <musthafafaishal@gmail.com>
2023-05-03 09:34:56 +0700

    refactor: change the file name

--- app/src/main/java/com/dicoding/storyapp/data/remote/response/StoryResponse.kt
+++ app/src/main/java/com/dicoding/storyapp/data/remote/response/StoryResponse.kt
@@ -2,7 +2,7 @@ package com.dicoding.storyapp.data.remote.response
 
 import com.google.gson.annotations.SerializedName
 
-data class ListStoryResponse(
+data class StoryResponse(
     @field:SerializedName("id")
     val id: String,
 
