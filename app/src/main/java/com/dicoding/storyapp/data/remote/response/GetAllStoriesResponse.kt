1c7ac11bd113d0b0fa9435bc06c681e82cbc1545
Musthafa Faishal <musthafafaishal@gmail.com>
2023-05-03 09:17:29 +0700

    build: create get all stories response model

---
+++ app/src/main/java/com/dicoding/storyapp/data/remote/response/GetAllStoriesResponse.kt
@@ -0,0 +1,14 @@
+package com.dicoding.storyapp.data.remote.response
+
+import com.google.gson.annotations.SerializedName
+
+data class GetAllStoriesResponse(
+    @field:SerializedName("error")
+    val error: Boolean,
+
+    @field:SerializedName("message")
+    val message: String,
+
+    @field:SerializedName("listStory")
+    val listStory: ArrayList<ListStoryResponse>
+)
