3efed38a769848fa059e3255894864fe1f120b47
Musthafa Faishal <musthafafaishal@gmail.com>
2023-05-03 09:56:39 +0700

    refactor: change the name file

--- app/src/main/java/com/dicoding/storyapp/data/remote/response/AllStoriesResponse.kt
+++ app/src/main/java/com/dicoding/storyapp/data/remote/response/AllStoriesResponse.kt
@@ -2,7 +2,7 @@ package com.dicoding.storyapp.data.remote.response
 
 import com.google.gson.annotations.SerializedName
 
-data class GetAllStoriesResponse(
+data class AllStoriesResponse(
     @field:SerializedName("error")
     val error: Boolean,
 
