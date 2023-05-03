db537d2d80a3b3d8188a6d23076c610b8e82034e
Musthafa Faishal <musthafafaishal@gmail.com>
2023-05-03 09:34:00 +0700

    refactor: make it simple in 1 file with same response model

--- app/src/main/java/com/dicoding/storyapp/data/remote/response/MessageResponse.kt
+++ app/src/main/java/com/dicoding/storyapp/data/remote/response/MessageResponse.kt
@@ -2,7 +2,7 @@ package com.dicoding.storyapp.data.remote.response
 
 import com.google.gson.annotations.SerializedName
 
-data class AddNewStoryResponse(
+data class MessageResponse(
     @field:SerializedName("error")
     val error: Boolean,
 
