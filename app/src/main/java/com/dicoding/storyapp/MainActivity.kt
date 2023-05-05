c0395eca7748d64661fecad9067dde311eb7ce5b
Musthafa Faishal <musthafafaishal@gmail.com>
2023-05-05 10:21:38 +0700

    refactor: load home fragment to main layout

--- app/src/main/java/com/dicoding/storyapp/MainActivity.kt
+++ app/src/main/java/com/dicoding/storyapp/MainActivity.kt
@@ -2,10 +2,24 @@ package com.dicoding.storyapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.dicoding.storyapp.ui.home.HomeFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragmentManager = supportFragmentManager
+        val homeFragment = HomeFragment()
+        val fragment = fragmentManager.findFragmentByTag(HomeFragment::class.java.simpleName)
+
+        if (fragment !is HomeFragment) {
+            Log.d("StoryApp", "Fragment Name: " + HomeFragment::class.java.simpleName)
+            fragmentManager
+                .beginTransaction()
+                .add(R.id.frame_container, homeFragment, HomeFragment::class.java.simpleName)
+                .commit()
+        }
     }
 }
