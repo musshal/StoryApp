9073df71b6e2241998950e022aa884cedfc442b6
Musthafa Faishal <musthafafaishal@gmail.com>
2023-05-07 17:46:56 +0700

    build: create camera activity

---
+++ app/src/main/java/com/dicoding/storyapp/ui/camera/CameraActivity.kt
@@ -0,0 +1,12 @@
+package com.dicoding.storyapp.ui.camera
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dicoding.storyapp.R
+
+class CameraActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_camera)
+    }
+}
